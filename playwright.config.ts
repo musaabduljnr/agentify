53650061fd8c9dd266407aaa6bdd9f5dc552703c
musaabduljnr <musaabduljnr@gmail.com>
2026-06-25 21:38:36 +0100

    chore: fix CI playwright timeouts by running e2e tests against production build

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: "npm run dev",
+    command: process.env.CI ? "npm run start" : "npm run dev",
     port: 3000,
     reuseExistingServer: !process.env.CI,
   },
